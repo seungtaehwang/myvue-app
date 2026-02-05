dcca23f9db92b28487c1f523a8b09a7285abe839
seungtaehwang <forlink@naver.com>
2026-02-05 12:42:10 +0900

    Add deployment config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue()],
   base: '/myvue-app/',
   resolve: {
     alias: {
