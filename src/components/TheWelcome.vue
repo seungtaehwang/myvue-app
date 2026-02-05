7fd701bc9022ceb0c0c23a1eb8e17334ab3f4502
seungtaehwang <forlink@naver.com>
2026-02-05 12:21:05 +0900

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,18 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+
+const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Documentation</template>
+
+    Welcomm React Portfolio - 황승태
+    <a href="https://seungtaehwang.github.io/react-app/">Lets go</a>
+  </WelcomeItem>
+</template>
